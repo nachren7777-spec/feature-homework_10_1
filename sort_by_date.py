082c72b6d6cf7c581210a2dd5db275cb0a7636b4
nachren7777-spec <nachren7777@gmail.com>
2026-06-09 12:27:32 +0500

    Добавлены файлы к дз

---
+++ sort_by_date.py
@@ -0,0 +1,24 @@
+from typing import List
+
+"""словарь по умолчанию"""
+default_dict: List[dict] = [
+                            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+
+def sort_by_date(default_dict) -> List[dict] | str:
+    order: str = input("Введите в каком порядке надо  сортировать список: По возрастанию/ По убыванию ")
+    if order == "По возрастанию" or order == "":
+        sorted_list = sorted(default_dict, key=lambda x: x['date'], reverse=True)
+        return sorted_list
+    if order == "По убыванию":
+        sorted_list = sorted(default_dict, key=lambda x: x['date'], reverse=False)
+        return sorted_list
+    else:
+        return "Не допустимый формат"
+
+
+sort_by_date(default_dict)
